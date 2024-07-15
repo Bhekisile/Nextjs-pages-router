08ebb2e558f3be2dc1d0b152b547b1ebc69a682f
Bhekisile <bhekisilejozi@gmail.com>
2024-07-15 11:05:15 +0200

    Define page title and favicon

--- pages/_app.js
+++ pages/_app.js
@@ -4,6 +4,8 @@ import Navigation from '@/components/Navigation';
 import Header from '@/components/Header';
 
 import { Josefin_Sans } from 'next/font/google';
+import Head from "next/head";
+import Link from "next/link";
 
 const josefin = Josefin_Sans({
   subsets: ['latin'],
@@ -12,17 +14,23 @@ const josefin = Josefin_Sans({
 
 export default function App({ Component, pageProps }) {
   return (
-    <div
-      className={`${josefin.className} antialiased bg-primary-950 text-primary-100 min-h-screen flex flex-col relative`}
-    >
-      <Header />
+    <>
+    <Head>
+      <title>The Wild Oasis</title>
+      <link rel="icon" href="logo.png" />
+    </Head>
+      <div
+        className={`${josefin.className} antialiased bg-primary-950 text-primary-100 min-h-screen flex flex-col relative`}
+      >
+        <Header />
 
-      <header className='flex-1 px-8 py-12 grid'>
-        <main className='max-w-7xl mx-auto w-full'>
-          {/* CONTENT GOES HERE */}
-          <Component {...pageProps} />;
-        </main>
-      </header>
-    </div>
+        <header className='flex-1 px-8 py-12 grid'>
+          <main className='max-w-7xl mx-auto w-full'>
+            {/* CONTENT GOES HERE */}
+            <Component {...pageProps} />;
+          </main>
+        </header>
+      </div>
+    </>
   )
 }
